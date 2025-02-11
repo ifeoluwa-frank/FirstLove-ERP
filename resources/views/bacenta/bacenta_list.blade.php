c1d6b767edcab6bb9d43866d54e9d627e3054373
Japheth Abashe <abashejaph@gmail.com>
2025-02-11 15:29:21 +0300

    updated bacenta & member

--- resources/views/bacenta/bacenta_list.blade.php
+++ resources/views/bacenta/bacenta_list.blade.php
@@ -164,7 +164,7 @@
 
                 <!-- Submit Button -->
                 <div class="text-right">
-                    <button type="submit" class="bg-yellow-500 text-white px-4 py-2 rounded">Submit</button>
+                    <button type="submit" class="w-full bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600 transition">Submit</button>
                 </div>
             </form>
         </div>
