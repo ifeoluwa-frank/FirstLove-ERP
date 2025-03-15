36ff9f51aac08882a736e3663193954d4f3aed24
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2025-03-15 08:34:32 +0100

    dashboard cleanup

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
         $members_count = Member::count();
         $bacenta_count = Bacenta::count();
         $sonta_count = Ministry::count();
-        $upcoming_birthdays = Member::select('first_name', 'last_name', 'bacenta_id', 'dob')
+        $upcoming_birthdays = Member::with('bacenta')->select('first_name', 'last_name', 'bacenta_id', 'dob')
         ->whereRaw("DATE_FORMAT(dob, '%m-%d') >= DATE_FORMAT(NOW(), '%m-%d')")
         ->orderByRaw("DATE_FORMAT(dob, '%m-%d') ASC")
         ->limit(3)
--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -97,14 +97,14 @@
                   <h2 class="text-2xl ml-2 mb-2">Birthdays</h2>
                   @forelse($upcoming_birthdays as $upcoming_birthday)
                     <div class="flex align-center justify-between mb-3">
-                      <div class="flex gap-2">
+                      <div class="flex gap-1">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="svgs">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg>
                                                   
                           <div class="mt-1 mr-3">
-                              <p class="text-xs font-bold">{{ $upcoming_birthday->first_name }} {{ $upcoming_birthday->last_name }}</p>
-                              <span class="text-xs text-gray-400">Bacenta - {{ $upcoming_birthday->bacenta_id }}</span>
+                              <p class="text-xs font-bold">{{ $upcoming_birthday->first_name }} {{ Str::limit($upcoming_birthday->last_name, 5) }}</p>
+                              <span class="text-xs text-gray-400">{{ $upcoming_birthday->bacenta->bacenta_name }}</span>
                           </div>
                       </div>
                       
@@ -112,7 +112,7 @@
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 svgs">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.871c1.355 0 2.697.056 4.024.166C17.155 8.51 18 9.473 18 10.608v2.513M15 8.25v-1.5m-6 1.5v-1.5m12 9.75-1.5.75a3.354 3.354 0 0 1-3 0 3.354 3.354 0 0 0-3 0 3.354 3.354 0 0 1-3 0 3.354 3.354 0 0 0-3 0 3.354 3.354 0 0 1-3 0L3 16.5m15-3.379a48.474 48.474 0 0 0-6-.371c-2.032 0-4.034.126-6 .371m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.169c0 .621-.504 1.125-1.125 1.125H4.125A1.125 1.125 0 0 1 3 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 0 1 6 13.12M12.265 3.11a.375.375 0 1 1-.53 0L12 2.845l.265.265Zm-3 0a.375.375 0 1 1-.53 0L9 2.845l.265.265Zm6 0a.375.375 0 1 1-.53 0L15 2.845l.265.265Z" />
                             </svg> 
-                          <span class="text-sm mt-1">{{ $upcoming_birthday->dob }}</span>
+                          <span class="text-sm mt-1">{{ \Carbon\Carbon::parse($upcoming_birthday->dob)->format('M d') }}</span>
                       </div>
                   </div>
                   @empty
