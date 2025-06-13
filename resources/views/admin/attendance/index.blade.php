04ef216dd96f74c895f9b0fd730e66701ede2b2c
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2025-06-13 08:33:15 +0100

    attendance

--- resources/views/admin/attendance/index.blade.php
+++ resources/views/admin/attendance/index.blade.php
@@ -99,7 +99,7 @@
                   </div>
               <div class="card">
                 <div class="card-content">
-                  @if($membershipAttendance->isNotEmpty())
+                  @if($membershipAttendance)
                     <div class="flex items-center justify-between">
                       <div class="widget-label">
                         <h3>Bacentas Attendance - {{ $membershipAttendance }}</h3>
