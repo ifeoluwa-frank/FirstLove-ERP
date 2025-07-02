1b759285546b55d51367d8265d5e315a22708f6c
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2025-07-02 21:04:43 +0100

    bacenta fix

--- resources/views/admin/member/details.blade.php
+++ resources/views/admin/member/details.blade.php
@@ -30,7 +30,7 @@
                   <div class="image w-48 h-48 mx-auto">
                     {{-- <img src="https://avatars.dicebear.com/v2/initials/john-doe.svg" alt="John Doe" class="rounded-full"> --}}
                     @if($member->profile_picture)
-                        <img src="{{ asset('storage/' . $member->profile_picture) }}" alt="Profile Picture" class="rounded-full">
+                        <img src="{{ asset('storage/' . $member->profile_picture) }}" alt="Profile Picture" class="rounded-full" width="150">
                     @else
                         <img src="https://avatars.dicebear.com/v2/initials/john-doe.svg" alt="John Doe" class="rounded-full">
                     @endif
