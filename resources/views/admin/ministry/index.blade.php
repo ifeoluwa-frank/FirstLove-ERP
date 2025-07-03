0b344ce2f1d5bd6bd698a091f2338cdd901125a3
Japheth Abashe <abashejaph@gmail.com>
2025-07-03 14:40:02 +0300

    button color fix

--- resources/views/admin/ministry/index.blade.php
+++ resources/views/admin/ministry/index.blade.php
@@ -145,7 +145,7 @@
                     </div>
                     <!-- Submit Button -->
                     <div class="text-right">
-                        <button type="submit" class="w-full bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600 transition">Submit</button>
+                        <button type="submit" class="w-full bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition">Submit</button>
                     </div>
                 </form>
             </div>
